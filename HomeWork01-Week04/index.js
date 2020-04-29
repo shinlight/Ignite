ba35f647a11e666da9cfb08287c284b26e919919
Igor <igor.bragato@gmail.com>
2020-04-29 17:39:03 +0200

    temp

--- HomeWork01-Week04/index.js
+++ HomeWork01-Week04/index.js
@@ -1,3 +1,15 @@
-let fetchExample async() => {
-    await fetch("http://igorbragato.com")
+let userName = "Igor";
+
+function showMessage() {
+    let message = 'Hello, ' + userName;
+    alert(message);
+  }
+
+function anotherFunction() {
+    
 }
+
+  function showMessage(from, text = anotherFunction()) {
+    // anotherFunction() only executed if no text given
+    // its result becomes the value of text
+  }
